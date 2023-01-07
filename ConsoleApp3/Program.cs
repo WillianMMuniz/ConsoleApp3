57b4309ba2e8f1319943f37f9c3df9dc765aa17e
willm <willm@DESKTOP-MMS3GO0>
2023-01-07 11:01:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ValorEmprestimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual o valor do emprestimo? ");
+            double valor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas parcelas? ");
+            int parcelas = int.Parse(Console.ReadLine());
+
+            double valorAPagar = (valor + (valor * 20) / 100);
+            Console.WriteLine("O valor a pagar é {0}", valorAPagar);
+
+            double emprestimo = (valorAPagar / parcelas);
+            Console.WriteLine("Vou pagar {0} de {1}", parcelas, emprestimo);
+        }
+    }
+}
